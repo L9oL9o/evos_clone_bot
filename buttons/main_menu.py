7c96b7d960d45a1785027bed23ccd425c451941e
Muhammadsodiq <ertye9020@gmail.com>
2023-12-03 10:45:53 +0500

    added functions such as: take id from the picture(this feature only for testing), and initial greeting

--- buttons/main_menu.py
+++ buttons/main_menu.py
@@ -1,12 +1,14 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 main_menu = ReplyKeyboardMarkup(
     [
-        # [KeyboardButton(text="Get Photo ID")],
+        [KeyboardButton(text="Get Photo ID")],
         [KeyboardButton(text="🏢 О компании"), KeyboardButton("📍 Филиалы")],
         [KeyboardButton(text="💼 Вакансии")],
         [KeyboardButton(text="📱 Меню"), KeyboardButton(text="🗣 Новости")],
         [KeyboardButton(text="📞 Контакты/Адрес"), KeyboardButton(text="🇺🇿/🇷🇺 Язык")],
 
     ], resize_keyboard=True)
+
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton)
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, ContentTypes)
 from dotenv import load_dotenv
 import os
 from buttons import main_menu
@@ -13,10 +13,23 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer(text="Can we help you ?", reply_markup=main_menu)
+    # await message.answer(text="w", reply_markup=main_menu)
+    await bot.send_photo(message.from_user.id,
+                         "AgACAgIAAxkBAAN9ZWwRW82ScHfx4akHj1o7RwltOokAAk3PMRuYQmFLHynYJ8VllUEBAAMCAANtAAMzBA",
+                         reply_markup=main_menu)
+
+
+@dp.message_handler(text="Get Photo ID")
+async def photo_data(message: types.Message):
+    await message.answer(text="Send a photo to get ID of this photo", reply_markup=main_menu)
+
+
+@dp.message_handler(content_types=ContentTypes.PHOTO)
+async def photo_id(message: types.Message):
+    await message.answer(message.photo[-1].file_id)
+    await message.answer("This is ID of sent photo in Telegram servers")
 
 
 if __name__ == '__main__':
